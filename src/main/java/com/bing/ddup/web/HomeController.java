c809a358e28a4154a87a6b789bc9624e7951daac
kevin2234 <1232234@gmail.com>
2017-07-19 18:23:10 +0800

    session test

--- src/main/java/com/bing/ddup/web/HomeController.java
+++ src/main/java/com/bing/ddup/web/HomeController.java
@@ -5,6 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Enumeration;
+
 @Controller
 @RequestMapping("/")
 public class HomeController {
@@ -15,15 +20,35 @@ public class HomeController {
 
     @RequestMapping("home")
     public String home(){
-        LOG.info("阿里巴巴最牛xxxxxxxxxxx！");
-        logger.info("阿里巴巴最牛xxxxxxxxxxx！");
-        System.out.println("hello home!");
+        LOG.info("hello home!");
+//        logger.info("hello home!");
         return "index";
     }
 
     @RequestMapping(value = "index", method = RequestMethod.GET)
-    public String index(Model model) {
-        System.out.println("hello index!");
+    public String index(Model model,
+                HttpServletRequest request, HttpServletResponse response) {
+        LOG.info("hi {}！", "hello index!");
+        HttpSession session = request.getSession();
+        logger.info("sessionid : {}", session.getId());
+        session.setAttribute("hello", "just a joke");
+        return "index";
+    }
+
+    @RequestMapping(value = "login")
+    public String login(Model model,
+                        HttpServletRequest request, HttpServletResponse response) {
+        //获取session
+        HttpSession session = request.getSession();
+        logger.info("sessionid : {}", session.getId());
+        //获取session中所有的键值
+        Enumeration<?> enumeration = session.getAttributeNames();
+        logger.info("session有值？ {}", enumeration.hasMoreElements());
+        while (enumeration.hasMoreElements()) {
+            String name = enumeration.nextElement().toString();
+            Object value = session.getAttribute(name);
+            logger.info("{}：{}", name, value);
+        }
         return "index";
     }
 }
